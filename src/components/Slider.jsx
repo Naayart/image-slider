90fe9079af8edd7e264f6fe507f6ef43cc96926a
Naayart <jerusha328@gmail.com>
2025-08-28 14:20:59 -0700

    initial commit

---
+++ src/components/Slider.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { descriptions, images } from "../data";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+
+const getRandomNumber = () => Math.floor(Math.random() * 41) -20;
+
+const Slider = () => {
+
+    const [index, setIndex] = useState (0);
+
+  return (
+    // Container
+    <div className="relative">
+      {/* Slider */}
+      <div className="flex flex-col items-center gap-x-20 lg:flex-row lg:items-start">
+        {/* Images */}
+        <div className="relative h-[300px] w-[300px] sm:h-[400px] sm:w-[400px]">
+          {images.map((image, i) => (
+            <img key={i} src={image} width={500} className={`absolute h-full w-full rounded-3xl object-cover transition-all duration-300 ${ i === index ? 'activeImage' : 'inActiveImage' }`}
+            style={{transform: `rotate(${index === i ? 0 : getRandomNumber()}deg)`}}/>
+          ))}
+        </div>
+        {/* Descriptions */}
+        <div className="relative sm:w-[400px] sm:h-[200px] w-[320px]  mt-22 lg:mt-5">
+          {descriptions.map((des, i) => (
+            <p key={i} className={`text-center sm:text-xl text-gray-600 absolute transition-all duration-300 ${i === index ? 'activeDes delay-200' : 'inActiveDes' }`} >{des}</p>
+          ))}
+        </div>
+      </div>
+      {/* Controls */}
+      <div className="absolute bottom-0 lg:-bottom-20 left-1/2 -translate-x-1/2 flex gap-x-5">
+        <button className="bg-gray-100 p-1.5 cursor-pointer rounded-full text-gray-600 hover:bg-gray-200 transition-colors " onClick={() => setIndex(prev => (prev === 0 ? images.length - 1 : prev - 1))}>
+            <ArrowLeft size={18} />
+        </button>
+        <button className="bg-gray-100 p-1.5 cursor-pointer rounded-full text-gray-600 hover:bg-gray-200 transition-colors " onClick={() => setIndex(prev => (prev === images.length - 1 ? 0 : prev + 1))}>
+            <ArrowRight size={18} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
---
+++ src/data/index.js
@@ -0,0 +1,15 @@
+import image1 from "../assets/images/image-1.jpg";
+import image2 from "../assets/images/image-2.jpg";
+import image3 from "../assets/images/image-3.jpg";
+import image4 from "../assets/images/image-4.jpg";
+import image5 from "../assets/images/image-5.jpg";
+
+export const images = [image1, image2, image3, image4, image5];
+
+export const descriptions = [
+  "Small🐶 breeds like Chihuahuas, Pomeranians, or Dachshunds are known for their playful, energetic, and loyal nature. Despite their size, many small dogs have big personalities and love attention. They make great companions and are often affectionate with their owners.",
+  "🐻s are large, powerful mammals found in forests, mountains, and even Arctic regions. There are different species like the grizzly bear, polar bear, and black bear. They are omnivores, eating fish, berries, honey, and even small animals. Some bears hibernate in winter to conserve energy.",
+  "The giant 🐼 is a black-and-white bear native to China. They mainly eat bamboo and are known for their cute, round faces and slow, peaceful nature. Despite their bear-like appearance, they are not aggressive and love climbing trees!",
+  "🦁Known as the 'King of the Jungle,' lions are powerful big cats that live in groups called prides. Found mainly in Africa, male lions are known for their majestic manes, while lionesses do most of the hunting. They are skilled hunters, preying on zebras, antelopes, and other animals.",
+  "The tallest land animal, 🦒 have long necks that help them reach tree leaves for food. Native to Africa, they have unique spotted coats and can run surprisingly fast despite their size. Their long legs also make them excellent kickers against predators like lions.",
+];
